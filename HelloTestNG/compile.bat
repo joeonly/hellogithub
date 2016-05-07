87f2736b33a85d74a2b3c213948baf9e0c8bfbcb
zhouzho <zhongli.zhou@hpe.com>
2016-05-08 01:25:29 +0800

    revision2

--- HelloTestNG/compile.bat
+++ HelloTestNG/compile.bat
@@ -1,5 +1,6 @@
 @echo off
 
+if not exist classes mkdir classes
 rem set JAVA_HOME=D:\sdk\jdk1.6.0_24
 set testng_basedir=D:\pnp\eclipse\eclipse4.4.0\plugins\org.testng.eclipse_6.9.11.201604020423\lib
 set _cmd=%JAVA_HOME%\bin\javac -cp %testng_basedir%\testng.jar;%testng_basedir%\jcommander.jar;%testng_basedir%\bsh-2.0b4.jar;%testng_basedir%\snakeyaml.jar
